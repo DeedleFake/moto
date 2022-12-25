f391c340a4c351a1f9b419bf21424beff30ced0a
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-24 23:36:46 -0500

    backend: just comment everything out for now

--- backend/backend.go
+++ backend/backend.go
@@ -1,12 +1,7 @@
 package backend
 
 import (
-	"image"
-	"image/color"
-	"os"
-
 	"golang.org/x/exp/maps"
-	"golang.org/x/image/math/f32"
 )
 
 var backends map[string]Backend
@@ -20,49 +15,29 @@ func Names() []string {
 }
 
 type Backend interface {
-	Start() error
-	Destroy()
-	Session() *Session
-	PresentationClock() *Clock
-	DRM() *os.File
-	BufferCaps() uint32
-}
-
-type Session struct {
-	// TODO
-}
-
-type Clock struct {
-	// TODO
+	//Start() error
+	//Destroy()
+	//Session() *Session
+	//PresentationClock() *Clock
+	//DRM() *os.File
+	//BufferCaps() uint32
 }
 
 type Renderer interface {
-	BindBuffer(*Buffer)
-	Begin(width, height uint32)
-	End()
-	Clear(color.Color)
-	Scissor(image.Rectangle)
-	RenderSubtextureWithMatrix(*Texture, image.Rectangle, f32.Mat3, float32) error
-	RenderQuadWithMatrix(color.Color, f32.Mat3)
-	ShmTextureFormats() []uint32
-	DmabufTextureFormats() DrmFormatSet
-	RenderFormats() DrmFormatSet
-	PreferredReadFormat() uint32
-	ReadPixels(data []byte, fmt, flags, stride, width, height uint32, src, dst image.Point) error
-	Destroy()
-	DRM() *os.File
-	RenderBufferCaps() uint32
-	TextureFromBuffer(*Buffer) *Texture
-}
-
-type Buffer struct {
-	// TODO
-}
-
-type Texture struct {
-	// TODO
-}
-
-type DrmFormatSet struct {
-	// TODO
+	//BindBuffer(*Buffer)
+	//Begin(width, height uint32)
+	//End()
+	//Clear(color.Color)
+	//Scissor(image.Rectangle)
+	//RenderSubtextureWithMatrix(*Texture, image.Rectangle, f32.Mat3, float32) error
+	//RenderQuadWithMatrix(color.Color, f32.Mat3)
+	//ShmTextureFormats() []uint32
+	//DmabufTextureFormats() DrmFormatSet
+	//RenderFormats() DrmFormatSet
+	//PreferredReadFormat() uint32
+	//ReadPixels(data []byte, fmt, flags, stride, width, height uint32, src, dst image.Point) error
+	//Destroy()
+	//DRM() *os.File
+	//RenderBufferCaps() uint32
+	//TextureFromBuffer(*Buffer) *Texture
 }
